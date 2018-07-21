cdf138c398950b84be6bfd792a509d54add41c7b
root <root@localhost.localdomain>
2018-07-22 02:44:45 +1000

    Added user input to MAC changer information is now stored in variables

--- mac_changer/mac_changer.py
+++ mac_changer/mac_changer.py
@@ -2,7 +2,31 @@
 
 import subprocess
 
-# Run linux commands through subprocess
-subprocess.call("ifconfig eth0 down", shell=True)
-subprocess.call("ifconfig eth0 hw ether 00:11:22:33:44:66", shell=True)
-subprocess.call("ifconfig eth0 up", shell=True)
+# Get info from user for command arguments
+import optparse
+
+# Create parser object to handle user input
+parser = optparse.OptionParser()
+
+# User arguments to expect from user (-i or interface acceptable arguments)
+parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
+parser.add_option("-m", "--mac", dest="new_mac", help="new MAC address")
+
+# Understand user arguments
+(options, arguments) = parser.parse_args()
+
+# User input
+interface = options.interface
+new_mac = options.new_mac
+
+print("[+] Changing MAC address for " + interface + " to " + new_mac)
+
+# Run linux commands through subprocess using default
+# subprocess.call("ifconfig " + interface + " down", shell=True)
+# subprocess.call("ifconfig " + interface + " hw ether " + new_mac, shell=True)
+# subprocess.call("ifconfig " + interface + " up", shell=True)
+
+# Run linux commands through subprocess more securely using a list
+subprocess.call(["ifconfig", interface, "down"])
+subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+subprocess.call(["ifconfig", interface, "up"])
