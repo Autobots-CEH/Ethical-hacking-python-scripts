0a0836715d3626496f2123d46e703f691147b8e6
root <root@localhost.localdomain>
2018-07-31 01:27:15 +1000

    Created request to braodcas mac asking for IP next will send a packet to recieve a response

--- mac_changer/network_scanner.py
+++ mac_changer/network_scanner.py
@@ -2,7 +2,23 @@
 
 import scapy.all as scapy
 
+
 def scan(ip):
-    scapy.arping(ip)
+    # Create ARP ip address
+    arp_request = scapy.ARP(pdst=ip)
+    arp_request.show()
+
+    # Create ethernet object
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    broadcast.show()
+
+    # Combine ip and broadcast mac
+    arp_request_broadcast = broadcast/arp_request
+
+    print(arp_request_broadcast.summary())
+    arp_request_broadcast.show()
+
+    # List all scapy fields
+    #scapy.ls(scapy.ARP())
 
 scan("10.0.2.1/24")
