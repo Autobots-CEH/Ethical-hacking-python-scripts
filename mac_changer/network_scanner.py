2a90ad6409ce99c256c7676ea88e8dd1bbd18aa9
root <root@localhost.localdomain>
2018-08-02 21:57:04 +1000

    Allowed scanner to search current network for other devices, target IP and MAC address are then displayed to screen

--- mac_changer/network_scanner.py
+++ mac_changer/network_scanner.py
@@ -6,19 +6,21 @@ import scapy.all as scapy
 def scan(ip):
     # Create ARP ip address
     arp_request = scapy.ARP(pdst=ip)
-    arp_request.show()
 
     # Create ethernet object
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
-    broadcast.show()
 
     # Combine ip and broadcast mac
     arp_request_broadcast = broadcast/arp_request
 
-    print(arp_request_broadcast.summary())
-    arp_request_broadcast.show()
+    answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose="false")[0]
 
-    # List all scapy fields
-    #scapy.ls(scapy.ARP())
+    # Print information header \t tab
+    print("IP\t\t\tMAC Address\n------------------------------------")
+
+    # Specify Target information from list
+    for element in answered_list:
+        # ip address / followed by mac address
+        print(element[1].psrc + "\t\t" + element[1].hwsrc)
 
 scan("10.0.2.1/24")
